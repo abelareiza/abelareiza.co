df82d596733d9c946e1833ed8c439939480af351
Abel <abelareiza@gmail.com>
2022-02-23 23:29:08 -0500

    portfolio total renovation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  purge: ["./**/*.html"],
-  content: [],
-  theme: {},
-  plugins: [],
-}
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@themesberg/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Display', serif",
+        merriweather: "'Merriweather Sans', sans-serif",
+      },
+    },
+  },
+  plugins: [require("@themesberg/flowbite/plugin")],
+};
