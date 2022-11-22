6014ba745f94ebf3c53097372137a247b8062bd6
Abel <abelareiza@gmail.com>
2022-11-22 12:44:04 -0500

    portfolio update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,19 @@ module.exports = {
     extend: {
       fontFamily: {
         playfair: "'Playfair Display', serif",
-        merriweather: "'Merriweather Sans', sans-serif",
+        satoshi: "'Satoshi', sans-serif",
+      },
+      animation: {
+        wiggle: "wiggle 0.5s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-15deg)" },
+          "50%": { transform: "rotate(15deg)" },
+        },
       },
     },
   },
+  darkMode: "class",
   plugins: [require("@themesberg/flowbite/plugin")],
 };
